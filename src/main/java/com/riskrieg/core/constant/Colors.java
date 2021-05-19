fb7f46633873330ac020b09eb7b9e9ee6b2620c9
Aaron Yoder <aaronjyoder@gmail.com>
2021-05-19 16:57:16 -0400

    Update colors to match new palette

--- src/main/java/com/riskrieg/core/constant/Colors.java
+++ src/main/java/com/riskrieg/core/constant/Colors.java
@@ -4,9 +4,13 @@ import java.awt.Color;
 
 public final class Colors {
 
-  public static final Color TEXT_COLOR = new Color(116, 88, 60);
-  public static final Color TERRITORY_COLOR = new Color(224, 218, 217);
-  public static final Color WATER_COLOR = new Color(189, 169, 153);
-  public static final Color BORDER_COLOR = new Color(116, 88, 60);
+  public static final Color BORDER_COLOR = new Color(116, 79, 40);
+  public static final Color TEXT_COLOR = new Color(116, 79, 40);
+
+  public static final Color TERRITORY_COLOR = new Color(224, 219, 227);
+  public static final Color LAND_COLOR = new Color(200, 183, 173);
+
+  public static final Color WATER_COLOR = new Color(192, 163, 146);
+  public static final Color CONNECTION_COLOR = new Color(148, 125, 111);
 
 }
