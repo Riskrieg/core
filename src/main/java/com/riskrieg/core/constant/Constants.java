aa2bf70d8d997daa8080e4e1c3d1861cb06a2949
Aaron J Yoder <aaronjyoder@gmail.com>
2021-11-21 02:38:05 -0500

    Add license notice

--- src/main/java/com/riskrieg/core/constant/Constants.java
+++ src/main/java/com/riskrieg/core/constant/Constants.java
@@ -1,3 +1,21 @@
+/**
+ *     Riskrieg, an open-source conflict simulation game.
+ *     Copyright (C) 2021  Aaron Yoder <aaronjyoder@gmail.com>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.riskrieg.core.constant;
 
 import com.riskrieg.core.constant.color.ColorBatch;
@@ -7,7 +25,7 @@ import com.riskrieg.core.constant.color.PlayerColor;
 public final class Constants {
 
   public static final String NAME = "Riskrieg";
-  public static final String VERSION = "2109.11b";
+  public static final String VERSION = "2111.21a";
   public static final String SAVE_FILE_EXT = ".json";
 
   public static final int MIN_PLAYERS = 2;
