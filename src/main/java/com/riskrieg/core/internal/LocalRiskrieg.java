aadd3ccc545c9162919d62d8884d4da5c562ad84
Aaron J Yoder <aaronjyoder@gmail.com>
2022-05-20 08:29:35 -0400

    Implement LocalRiskrieg

--- src/main/java/com/riskrieg/core/internal/LocalRiskrieg.java
+++ src/main/java/com/riskrieg/core/internal/LocalRiskrieg.java
@@ -22,13 +22,19 @@ import com.riskrieg.core.api.Riskrieg;
 import com.riskrieg.core.api.group.Group;
 import com.riskrieg.core.api.identifier.GroupIdentifier;
 import com.riskrieg.core.api.requests.GameAction;
+import com.riskrieg.core.internal.group.LocalGroup;
+import com.riskrieg.core.internal.requests.GenericAction;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 
-public record LocalRiskrieg(Path repository) implements Riskrieg { // TODO: Implement
+public record LocalRiskrieg(Path repository) implements Riskrieg {
 
   public LocalRiskrieg {
     Objects.requireNonNull(repository);
@@ -40,25 +46,52 @@ public record LocalRiskrieg(Path repository) implements Riskrieg { // TODO: Impl
   @NonNull
   @Override
   public GameAction<Group> createGroup(GroupIdentifier identifier) {
-    return null;
+    Path groupPath = repository.resolve(identifier.id());
+    try {
+      Files.createDirectories(groupPath);
+      return new GenericAction<>(new LocalGroup(groupPath));
+    } catch (IOException e) {
+      return new GenericAction<>(e);
+    }
   }
 
   @NonNull
   @Override
   public GameAction<Group> retrieveGroup(GroupIdentifier identifier) {
-    return null;
+    Path groupPath = repository.resolve(identifier.id());
+    if (Files.exists(groupPath)) {
+      return new GenericAction<>(new LocalGroup(groupPath));
+    }
+    return new GenericAction<>(new NoSuchFileException("The group with the specified identifier does not exist"));
   }
 
   @NonNull
   @Override
   public GameAction<Collection<Group>> retrieveAllGroups() {
-    return null;
+    try (var elements = Files.list(repository)) {
+      List<Group> result = elements.filter(Files::isDirectory)
+          .map(LocalGroup::new)
+          .map(Group.class::cast)
+          .toList();
+      return new GenericAction<>(result);
+    } catch (IOException e) {
+      return new GenericAction<>(e);
+    }
   }
 
   @NonNull
   @Override
   public GameAction<Boolean> deleteGroup(GroupIdentifier identifier) {
-    return null;
+    Path groupPath = repository.resolve(identifier.id());
+    try (var paths = Files.walk(groupPath)) { // Delete everything within the group first.
+      final List<Path> pathsToDelete = paths.sorted(Comparator.reverseOrder()).toList();
+      for (Path path : pathsToDelete) { // Doing it this way in order to catch the exception more easily.
+        Files.deleteIfExists(path);
+      }
+      return new GenericAction<>(Files.deleteIfExists(groupPath)); // Delete the group
+    } catch (IOException e) {
+      return new GenericAction<>(false, e);
+    }
   }
 
 }
